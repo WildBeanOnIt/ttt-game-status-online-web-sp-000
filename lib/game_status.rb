55e610cb43239596aca2d40e553cdf22f9a93007
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-05-07 22:51:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,7 +3,17 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-ç
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],  #top row
+  [3,4,5],  #middle row
+  [6,7,8],  #bottom row
+  [0,3,6],  #top left straight down to bottm left
+  [1,4,7],  #top middle straight down to bottom middle
+  [2,5,8],  #top right straigh down to bottom right
+  [0,4,8],  #diagonal top left to bottom right
+  [2,4,6]  #diagonal top right to bottom left
+]
 
 def won?(board)
   
